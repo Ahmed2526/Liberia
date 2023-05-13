5c5709e7a9522de51b7d2d3ae52244f22b022c2f
AhmedEbrahem <ahmedebrahem835@gmail.com>
2023-05-13 21:06:46 +0300

    13-5

---
+++ Liberia/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Liberia.Data;
+
+namespace Liberia.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            //TODO: use ViewModel
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+
+
+    }
+}
--- Liberia/GlobalUsings.cs
+++ Liberia/GlobalUsings.cs
@@ -1,7 +1,7 @@
-﻿
-global using System.ComponentModel.DataAnnotations;
+﻿global using Microsoft.AspNetCore.Mvc;
 global using DAL.Models;
 global using DAL.ViewModels;
+
 
 
 
